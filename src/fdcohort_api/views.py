d0f7a2c79dd3219d65a9ae0059120d9a60ee7f56
kpnemre <kpnemre51@gmail.com>
2021-01-17 15:14:07 +0300

    Get json via JsonResponse

--- src/fdcohort_api/views.py
+++ src/fdcohort_api/views.py
@@ -11,14 +11,25 @@ def home_api(request):
     }
     return JsonResponse(data)
 
+# 1. yöntem Jsonresponse kullanıp queryset elde edip for ile dönüp manuel Json üretmek 
+
+
 def student_list_api(request):
     if request.method =='GET':
         students = Student.objects.all()
         students_count = Student.objects.count()
          # TypeError: Object of type QuerySet is not JSON serializable 
         #  json objesine çeviremiyor. for ile tek tek ulaşmak gerekiyor
+        student_list =[]
+        for student in students:
+            student_list.append({
+                "firstname":student.first_name,
+                "lastname":student.last_name,
+                "number":student.number,
+            })
         data ={
-            "students":students,
+            # "students":students,
+            "students":student_list,
             "count":students_count,
         }
         return JsonResponse(data)
