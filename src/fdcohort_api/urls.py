5a5e9f41a77817443fdbe451666bb073faee13b8
kpnemre <kpnemre51@gmail.com>
2021-01-20 13:10:45 +0300

    Add delete,update,get functions

--- src/fdcohort_api/urls.py
+++ src/fdcohort_api/urls.py
@@ -2,13 +2,15 @@
 
 from django.urls import path
 from django.urls.conf import include
-from .views import home_api
+from .views import home_api, student_list_create_api, student_get_update_delete
 
 # , student_list_api, student_create_api
 
 urlpatterns = [
    
     path('home-api/', home_api),
+    path('list-create-api/',student_list_create_api),
+    path('<int:id>/',student_get_update_delete, name="detail"),
     # path('list-api/', student_list_api),
     # path('create-api/', student_create_api),
 
--- src/fdcohort_api/views.py
+++ src/fdcohort_api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import JsonResponse
 from fscohort.models import Student
 from django.core.serializers import serialize
@@ -6,6 +6,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from .serializers import StudentSerializer
 # Create your views here.
 def home_api(request):
@@ -18,6 +19,7 @@ def home_api(request):
 
 # 3. restframework
 @api_view(["GET", "POST"])
+# Browserable api kullanmak için yazdık. 
 def student_list_create_api(request):
     if request.method=="GET":
         students=Student.objects.all()
@@ -27,11 +29,50 @@ def student_list_create_api(request):
     elif request.method=="POST":
         serializer= StudentSerializer(data=request.data)
         # post_data= json.loads(request.body) burdaki işlemi yapıyor
-        
+        if serializer.is_valid():
+            # kendiliğinden default valdation sağlıyor
+            # student=form.save(commit=False)
+            # student.teacher=request.user
+            # student.save()
+            # Bu işlem yerine:
+            # serializer.save(teacher=request.user)
+            serializer.save()
+            data={
+                "message":"Student created successfully.. "
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(["GET", "PUT","DELETE"])#decorater
+def student_get_update_delete(request,id):
+    # pk: primary key
+    student= get_object_or_404(Student, id=id)
+    if request.method=="GET":
+        serializer= StudentSerializer(student)
+        return Response(serializer.data)
+    if request.method=="PUT":
+        serializer= StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data={
+                "message":"Student updated succcesffuly"
+            }
+            return Response(data)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method=="DELETE":
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+       
+            
+        
+    
+        
+    
 
 
 # 2. yöntem serialize kullanmak . bu queryset i otomatik istenen formata çeviriyor. Sözlük üzerinde çalışmamız gerek. 
+# validation yazmadık
 # def student_list_api(request):
 #     if request.method =='GET':
 #         students = Student.objects.all()
